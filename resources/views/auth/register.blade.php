7576dbf9368e46f2e09d2e86ca8bc72fe8b5a4d7
HIRORORO\user <hiro.0274417@gmail.com>
2026-08-07 00:57:18 +0900

    Post・ログイン・新規登録画面にエラー表示を追加

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,42 +1,122 @@
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 <head>
-    <title>新規登録</title>
+    <meta charset="UTF-8">
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
+
+    <title>新規ユーザー登録</title>
+
+    <link
+        rel="stylesheet"
+        href="{{ asset('css/style.css') }}"
+    >
 </head>
-<body>
-    <h1>新規ユーザー登録</h1>
 
-    <!-- フォーム開始：送信先(url)を '/register' に指定 -->
-    {!! Form::open(['url' => '/register']) !!}
+<body class="auth-page">
 
-        <div>
-            <label>ユーザー名</label>
-            {!! Form::text('username', null, ['class' => 'input']) !!}
-        </div>
+    <main class="auth-container">
 
-        <div>
-            <label>メールアドレス</label>
-            {!! Form::email('email', null, ['class' => 'input']) !!}
-        </div>
+        <h1 class="auth-title">
+            新規ユーザー登録
+        </h1>
 
-        <div>
-            <label>パスワード</label>
-            {!! Form::password('password', ['class' => 'input']) !!}
-        </div>
+        {{-- バリデーションエラー --}}
+        @if ($errors->any())
+            <div class="validation-errors">
+                @foreach ($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                @endforeach
+            </div>
+        @endif
 
-        <div>
-            <label>パスワード確認</label>
-            {!! Form::password('password_confirmation', ['class' => 'input']) !!}
-        </div>
+        <form
+            action="{{ url('/register') }}"
+            method="POST"
+            class="auth-form"
+        >
+            @csrf
 
-        <div>
-            {!! Form::submit('新規登録') !!}
-        </div>
+            <div class="auth-form-row">
 
-    {!! Form::close() !!}
+                <label for="username">
+                    ユーザー名
+                </label>
 
-    <!--ログイン画面へのリンク-->
-    <p><a href="/login">ログイン画面に戻る</a></p>
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value="{{ old('username') }}"
+                    autocomplete="username"
+                >
+
+            </div>
+
+            <div class="auth-form-row">
+
+                <label for="email">
+                    メールアドレス
+                </label>
+
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value="{{ old('email') }}"
+                    autocomplete="email"
+                >
+
+            </div>
+
+            <div class="auth-form-row">
+
+                <label for="password">
+                    パスワード
+                </label>
+
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    autocomplete="new-password"
+                >
+
+            </div>
+
+            <div class="auth-form-row">
+
+                <label for="password_confirmation">
+                    パスワード確認
+                </label>
+
+                <input
+                    type="password"
+                    id="password_confirmation"
+                    name="password_confirmation"
+                    autocomplete="new-password"
+                >
+
+            </div>
+
+            <button
+                type="submit"
+                class="auth-submit-button"
+            >
+                新規登録
+            </button>
+
+        </form>
+
+        <p class="auth-link">
+            <a href="{{ url('/login') }}">
+                ログイン画面に戻る
+            </a>
+        </p>
+
+    </main>
 
 </body>
 </html>

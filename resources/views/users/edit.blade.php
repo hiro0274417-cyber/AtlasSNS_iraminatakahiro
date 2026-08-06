ba132167f6698746b103f37130dcd18b904d570a
HIRORORO\user <hiro.0274417@gmail.com>
2026-08-06 17:06:36 +0900

    自動生成ファイルをGit管理対象から除外

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,4 +1,4 @@
-<!-- @extends('layouts.app')
+@extends('layouts.app')
 
 @section('content')
 
@@ -119,4 +119,3 @@
 </div>
 
 @endsection
- -->
--- storage/framework/views/625cf54941aa1fc6cd5ad111a84b4b4c169b347b.php
+++
@@ -1,5 +0,0 @@
-<h1>登録が完了しました</h1>
-<p>ようこそ、<?php echo e(session('username')); ?> さん！</p>
-
-<p><a href="/login">ログインページへ</a></p>
-<?php /**PATH C:\Users\user\Desktop\cmder\AtlasSNS_iraminatakahiro\resources\views/auth/added.blade.php ENDPATH**/ ?>
